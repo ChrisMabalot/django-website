bd461a620de29d4eccdba40086eb6c39dbde57c6
ChrisMabalot <CMabalot2000@gmail.com>
2022-12-23 19:09:49 +0800

    Reverted to old working url paths.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,5 +21,5 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', TemplateView.as_view(template_name = 'index.html'), name='index')
+    path('', views.index, name='index')
 ]
