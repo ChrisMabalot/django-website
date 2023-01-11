c5fd65fcf4c572f224dcc121d2e66153396bfa96
ChrisMabalot <CMabalot2000@gmail.com>
2023-01-11 19:43:04 +0800

    Added 0.0.0.0 to allowed_hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.chrisjaymabalot.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.chrisjaymabalot.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
