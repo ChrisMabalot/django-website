20ff980ec270aeece230cd5b2d566c6eb00c6ff3
ChrisMabalot <CMabalot2000@gmail.com>
2023-01-12 23:35:35 +0800

    Settings changes.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,11 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 
 from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
 from django.core.management.utils import get_random_secret_key 
-
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +29,7 @@ SECRET_KEY = get_random_secret_key() # os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.chrisjaymabalot.com', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
